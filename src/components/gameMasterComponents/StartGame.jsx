f1226f5a57357750c644b9d1daa9943d944670e6
Eszter Kobor <kobor.eszter@gmail.com>
2020-06-17 09:32:48 +0200

    Fix create crud method

--- src/components/gameMasterComponents/StartGame.jsx
+++ src/components/gameMasterComponents/StartGame.jsx
@@ -3,76 +3,73 @@ import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
 import styled from "styled-components";
-import { dbRefobject } from "../../database.js";
+import { appFirebase } from "../../database.js";
 import { GameContext } from "../contextProviders/GameProvider";
 import { GamePhaseContext } from "../contextProviders/GamePhaseProvider";
 import { gamePhases } from "../../gamePhasesObject";
 
 const Styles = styled.div`
-    .container {
-        background-color: rgba(255, 255, 255, 0.9);
-        padding: 30, 30, 30, 30;
-        margin: 100;
-        border-radius: 3px;
-    }
+  .container {
+    background-color: rgba(255, 255, 255, 0.9);
+    padding: 30, 30, 30, 30;
+    margin: 100;
+    border-radius: 3px;
+  }
 `;
 
 export default function StartGame() {
-    const [name, setName] = useState("");
+  const [name, setName] = useState("");
 
-    const generateId = () => {
-        return Math.floor(Math.random() * 10000);
-    };
-    const gameId = generateId();
+  const generateId = () => {
+    return Math.floor(Math.random() * 10000);
+  };
+  const gameId = generateId();
 
-    const setGame = useContext(GameContext)[1];
+  const setGame = useContext(GameContext)[1];
 
-    const setGamePhase = useContext(GamePhaseContext)[1];
+  const setGamePhase = useContext(GamePhaseContext)[1];
 
-    const saveName = (e) => {
-        setName(e.target.value);
-    };
+  const saveName = (e) => {
+    setName(e.target.value);
+  };
 
-    const handleNewGameCreation = (err) => {
-        if (!!err) {
-            console.log(err);
-        } else {
-            console.log("Successfully created");
-            setGame({ gameMaster: name, gameId: gameId });
-            //setGamePhase(gamePhases.waitingRoom);
-        }
-    };
+  const handleNewGameCreation = (err) => {
+    if (!!err) {
+      console.log(err);
+    } else {
+      console.log("Successfully created");
+      setGame({ gameMaster: name, gameId: gameId });
+      setGamePhase(gamePhases.waitingRoom);
+    }
+  };
 
-    const createNewGame = () => {
-        if (name !== "") {
-            let body = {
-                gameMaster: name,
-                gameId: gameId,
-            };
-            //appFirebase.databaseApi.create("games", body, handleNewGameCreation);
-            dbRefobject.ref("games").push(body);
+  const createNewGame = () => {
+    if (name !== "") {
+      let body = {
+        gameMaster: name,
+      };
+    appFirebase.databaseApi.create(`games/${gameId}`, body, handleNewGameCreation);
+    }
+  };
 
-        }
-    };
-
-    return (
-        <Styles>
-            <Container>
-                <h4>Start a new game as a game master</h4>
-                <Form>
-                    <Form.Group controlId="formGameMasterName">
-                        <Form.Label>Your name</Form.Label>
-                        <Form.Control
-                            onChange={saveName}
-                            type="text"
-                            placeholder="Your name that will appear during the game"
-                        />
-                    </Form.Group>
-                    <Button variant="warning" type="submit" onClick={createNewGame}>
-                        Start a new game
-                    </Button>
-                </Form>
-            </Container>
-        </Styles>
-    );
+  return (
+    <Styles>
+      <Container>
+        <h4>Start a new game as a game master</h4>
+        <Form>
+          <Form.Group controlId='formGameMasterName'>
+            <Form.Label>Your name</Form.Label>
+            <Form.Control
+              onChange={saveName}
+              type='text'
+              placeholder='Your name that will appear during the game'
+            />
+          </Form.Group>
+          <Button variant='warning' onClick={createNewGame}>
+            Start a new game
+          </Button>
+        </Form>
+      </Container>
+    </Styles>
+  );
 }
--- src/database.js
+++ src/database.js
@@ -1,5 +1,9 @@
 import firebase from "firebase";
-const firebaseConfig = {
+
+export let appFirebase = {};
+
+(function () {
+  const firebaseConfig = {
     apiKey: "AIzaSyAAAferugrYAbHqY0d96ondBPmqdPdaN_w",
     authDomain: "famous-person-guessing-game.firebaseapp.com",
     databaseURL: "https://famous-person-guessing-game.firebaseio.com",
@@ -8,7 +12,39 @@ const firebaseConfig = {
     messagingSenderId: "275457808030",
     appId: "1:275457808030:web:40675b36590f243c6c4d8f",
     measurementId: "G-1TFM95J5GM",
-};
+  };
 
-firebase.initializeApp(firebaseConfig);
-export const dbRefobject = firebase.database();
+  firebase.initializeApp(firebaseConfig);
+  appFirebase = firebase;
+
+  function fnCreate(path, body, callback) {
+    if (!path || !body) return;
+    appFirebase.database().ref(path).set(body, callback);
+  }
+
+  const fnRead = (path, successFunction, errorFunction) => {
+    if (!path) return;
+    appFirebase
+      .database()
+      .ref(path)
+      .on("value")
+      .then(successFunction, errorFunction);
+  };
+
+  const fnUpdate = (path, body, callback) => {
+    if (!path || !body) return;
+    appFirebase.database().ref(path).update(body, callback);
+  };
+
+  const fnDelete = (path, callback) => {
+    if (!path) return;
+    appFirebase.database().ref(path).remove(callback);
+  };
+
+  appFirebase.databaseApi = {
+    create: fnCreate,
+    read: fnRead,
+    delete: fnDelete,
+    update: fnUpdate,
+  };
+})();
