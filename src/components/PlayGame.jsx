888f4ec34223fe17c9da67fad94b2d64c4a07637
KlaraHajdu <49884249+KlaraHajdu@users.noreply.github.com>
2020-06-24 15:36:27 +0200

    Add turninfo data to batabase

--- src/components/PlayGame.jsx
+++ src/components/PlayGame.jsx
@@ -9,38 +9,50 @@ import TeamContainer from "./TeamContainer.jsx";
 function PlayGame() {
     const [game, setGame] = useContext(GameContext);
 
-    const [teamOnTurn, setTeamOnTurn] = useState("");
-
-    const [playerOnTurn, setPlayerOnTurn] = useState("");
-
-    const handlePlayerOnTurnResult = (snapshot) => {
-        setPlayerOnTurn(snapshot.val());
+    const handleTeamOnTurnResult = (snapshot) => {
+        setGame({ ...game, teamOnTurn: snapshot.val() });
+    };
+    const handleGreenPlayerOnTurnIndexResult = (snapshot) => {
+        let playerOnTurnIndex = snapshot.val();
+        if (game.teams) setGame({ ...game, playerOnTurn: game.teams.greenTeam[playerOnTurnIndex] });
     };
 
-    const handleTeamOnTurnResult = (snapshot) => {
-        setTeamOnTurn(snapshot.val());
+    const handleBluePlayerOnTurnIndexResult = (snapshot) => {
+        let playerOnTurnIndex = snapshot.val();
+        if (game.teams) setGame({ ...game, playerOnTurn: game.teams.blueTeam[playerOnTurnIndex] });
     };
 
     useEffect(() => {
+        if (game.ownName === game.gameMaster) {
+            appFirebase.databaseApi.create(`games/${game.gameId}/teamOnTurn`, "greenTeam");
+            appFirebase.databaseApi.create(`games/${game.gameId}/greenTeamTurnIndex`, "0");
+        }
         appFirebase.databaseApi.readOn(`games/${game.gameId}/teamOnTurn`, handleTeamOnTurnResult);
-        appFirebase.databaseApi.readOn(`games/${game.gameId}/playerOnTurn`, handlePlayerOnTurnResult);
+        appFirebase.databaseApi.readOn(
+            `games/${game.gameId}/greenTeamTurnIndex`,
+            handleGreenPlayerOnTurnIndexResult
+        );
+        appFirebase.databaseApi.readOn(
+            `games/${game.gameId}/blueTeamTurnIndex`,
+            handleBluePlayerOnTurnIndexResult
+        );
     }, []);
 
     return (
         <div>
             <Row>
                 <Col xs={12} md={3}>
-                    <TeamContainer team="blueTeam"/>
+                    <TeamContainer team="blueTeam" />
                 </Col>
                 <Col xs={12} md={6}>
                     <div className="team-data">
-                        <PhaseHeader title="Guessing"/>
-                        <div>Now the {teamOnTurn} team is guessing</div>
-                        <div>It is {playerOnTurn}'s turn now</div>
+                        <PhaseHeader title="Guessing" />
+                        <div>Now the {game.teamOnTurn} team is guessing</div>
+                        <div>It is {game.playerOnTurn}'s turn now</div>
                     </div>
                 </Col>
                 <Col xs={12} md={3}>
-                <TeamContainer team="greenTeam"/>
+                    <TeamContainer team="greenTeam" />
                 </Col>
             </Row>
         </div>
