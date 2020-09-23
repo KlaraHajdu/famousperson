2054511c9d2bd64bcbaa2c319a35636eafea1c84
KlaraHajdu <49884249+KlaraHajdu@users.noreply.github.com>
2020-09-23 10:55:57 +0200

    Delete clatter

--- src/components/GuessWord.jsx
+++ src/components/GuessWord.jsx
@@ -10,28 +10,21 @@ import { GameContext } from "./contextProviders/GameProvider";
 import { RoundContext } from "./contextProviders/RoundProvider";
 import { ScoreContext } from "./contextProviders/ScoreProvider";
 
-
 export default function GuessWord(props) {
     const [word, setWord] = useState();
     const score = useContext(ScoreContext)[0];
     const game = useContext(GameContext)[0];
     const round = useContext(RoundContext)[0];
 
-
     const selectRandomWord = (snapshot) => {
-        
         if (snapshot.val() !== null) {
             let words = snapshot.val();
             let randomId = Math.floor(Math.random() * Object.keys(words).length);
             setWord(Object.keys(words)[randomId]);
             let updateO = {};
             updateO[Object.keys(words)[randomId]] = null;
-            appFirebase.databaseApi.update(`games/${game.gameId}/${round}round`,
-            updateO,
-            updateDone
-            );
-        }
-        else props.endRound();
+            appFirebase.databaseApi.update(`games/${game.gameId}/${round}round`, updateO, updateDone);
+        } else props.endRound();
     };
     const updateDone = (err) => {
         if (!!err) {
@@ -59,23 +52,27 @@ export default function GuessWord(props) {
 
     useEffect(() => {
         appFirebase.databaseApi.readOnce(`games/${game.gameId}/${round}round`, selectRandomWord);
-        console.log("Guessword mounted")
-    }, [ score]);
-
-    useEffect(() => {}, [score]);
+        console.log("Guessword mounted");
+    }, [score]);
 
     return (
         <Row>
             <Col>
                 <h2>
-                <div style={{ display: "flex", justifyContent: "center", alignItems: "center", alignSelf:"center" }}>
-                        <Badge style={{ height: 36 }}  variant="warning">{word}</Badge>
-                        </div>
+                    <div
+                        style={{ display: "flex", justifyContent: "center", alignItems: "center", alignSelf: "center" }}
+                    >
+                        <Badge style={{ height: 36 }} variant="warning">
+                            {word}
+                        </Badge>
+                    </div>
                 </h2>
             </Col>
             <Col>
-                <div style={{ display: "flex", justifyContent: "center", alignItems: "center", alignSelf:"center" }}>
-                    <Button style={{ height: 36 }} onClick={scoreWordGuessed}>Guessed</Button>
+                <div style={{ display: "flex", justifyContent: "center", alignItems: "center", alignSelf: "center" }}>
+                    <Button style={{ height: 36 }} onClick={scoreWordGuessed}>
+                        Guessed
+                    </Button>
                 </div>
             </Col>
         </Row>
