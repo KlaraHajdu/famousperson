dfc91304ad3532f5636c1266cfc6ce50b6a94b1a
KlaraHajdu <49884249+KlaraHajdu@users.noreply.github.com>
2020-10-01 11:01:23 +0200

    Change favicon settings

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -51,7 +51,7 @@ export default function Header() {
         setGamePhase(gamePhases.startGame);
     };
 
-    const joinNewGame = () => {
+    const joinNewGame = (e) => {
         setGamePhase(gamePhases.joinGame);
     };
 
