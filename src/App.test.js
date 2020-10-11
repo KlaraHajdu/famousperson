3a1b8adbb563695663fea26aea000fa0046f202e
Peresztegi Adam <flash42@gmail.com>
2020-10-11 19:24:57 +0200

    Wrap <App /> in a redux Provider
    This is needed as the application uses redux in it's component tree. They *need* to connect to the store, which is made available by redux Provider

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import { Provider } from 'react-redux'
+import configureStore from 'redux-mock-store'
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const mockStore = configureStore()
+  const store = mockStore({})
+  const { getByText } = render(<Provider store={store}><App /></Provider>)
+  const linkElement = getByText(/Play/);
   expect(linkElement).toBeInTheDocument();
 });
