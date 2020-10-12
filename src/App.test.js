f58df5ffe2969d5b456977815f3adae1aca7db6d
Peresztegi Adam <flash42@gmail.com>
2020-10-12 17:14:28 +0200

    Add start game test

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,9 @@ import { render } from '@testing-library/react';
 import App from './App';
 import { Provider } from 'react-redux'
 import configureStore from 'redux-mock-store'
+import StartGame from './components/gameMasterComponents/StartGame'
+import { GamePhaseProvider } from "./components/contextProviders/GamePhaseProvider";
+
 
 test('renders learn react link', () => {
   const mockStore = configureStore()
@@ -11,3 +14,12 @@ test('renders learn react link', () => {
   const linkElement = getByText(/Play/);
   expect(linkElement).toBeInTheDocument();
 });
+
+
+test("shows the text for starting the game", () => {
+  const mockStore = configureStore()
+  const store = mockStore({})
+  const { getByText } = render(<Provider store={store}><GamePhaseProvider><StartGame /></GamePhaseProvider></Provider>);  
+  const linkElement = getByText(/Start a new game as a game master/);
+  expect(linkElement).toBeInTheDocument(); 
+  });
