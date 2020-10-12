1b2f17013967c5ef2f139a2e95e3eecef27b82bd
KlaraHajdu <49884249+KlaraHajdu@users.noreply.github.com>
2020-10-12 10:37:31 +0200

    Add provider to apptest

--- src/__tests__/apptest.js
+++ src/__tests__/apptest.js
@@ -1,10 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "../app";
+import { createStore } from 'redux';
+import allReducers from '../reducers/allReducers';
+import { Provider } from 'react-redux';
+
+
+const store = createStore(allReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+      </Provider>,
+    
+    div);
   ReactDOM.unmountComponentAtNode(div);
 });
